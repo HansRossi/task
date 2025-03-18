bc7086c32fcd8fa2e567594fdd7f318b528ab3d7
Giovanni <ivanalipov4@gmail.com>
2025-03-18 09:43:45 +0100

    all donneee

---
+++ task2.py
@@ -0,0 +1,3 @@
+firstNumber = float(input("Enter first number: "))
+secondNumber = float(input("Enter second number: "))
+print(f"Sum of number is {(firstNumber + secondNumber):.2f}, difference is {(firstNumber - secondNumber):.2f}, product is {(firstNumber * secondNumber):.2f} and division is {(firstNumber / secondNumber):.2f}.")
